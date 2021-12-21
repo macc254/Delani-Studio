caf607fe9f30f5186826beaeaf758084486a0f68
Mercy <mercycherotich757@gmail.com>
2021-12-21 15:38:17 +0300

    achieve a display o hover for the first project using jqueryand styled the displayed text to be white,

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//what we do on click diplays description
 $(document).ready(function() {
     $('.back').hide();
 
@@ -11,5 +12,21 @@ $(document).ready(function() {
     $('.back', '.flashcard').on('click', function() {
         $(this).hide();
         $(this).siblings('.front').show();
+    });
+});
+
+
+//portfolios on hover displays text
+$(document).ready(function() {
+    $('#port a img').hover(function() {
+        $(this).stop().animate({
+            opacity: .7
+        }, 700);
+        $('.text').removeClass('hide');
+    }, function() {
+        $(this).stop().animate({
+            opacity: 1
+        }, 900);
+        $('.text').addClass('hide');
     });
 });
